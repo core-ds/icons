dda60d423ec2b3c695353a674209829b70499931
fulcanellee <45999900+fulcanellee@users.noreply.github.com>
2025-08-04 13:50:28 +0700

    Fix/meta (#33)
    * fix: escape Unicode symbols
    
    * feat: add manual ci workflow
    
    ---------
    
    Co-authored-by: core-ds-bot <ds@gitmax.tech>

--- scripts/utils/fs.ts
+++ scripts/utils/fs.ts
@@ -1,8 +1,17 @@
 import { writeFileSync } from 'fs';
 
+const replacer = (_: unknown, value: unknown) => {
+    if (typeof value === 'string') {
+        // LSEP, NBSP
+        return value.replace(/[\u2028\u00a0]/g, ' ');
+    }
+
+    return value;
+};
+
 export const writeJSON = (filePath: string, data: unknown): void => {
     try {
-        const json = JSON.stringify(data, null, 2);
+        const json = JSON.stringify(data, replacer, 2);
         writeFileSync(filePath, json);
     } catch (error) {
         console.error(`Failed to write JSON to ${filePath}:`, error);
